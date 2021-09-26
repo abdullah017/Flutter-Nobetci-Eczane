c14935b48f4060e32a17dc09cfc2b9c8d6d1421c
Abdullah Taş <abdullahozvanli@gmail.com>
2021-09-26 15:50:08 +0300

    Update get_pharmacy.dart

--- lib/app/data/provider/local/get_pharmacy.dart
+++ lib/app/data/provider/local/get_pharmacy.dart
@@ -14,7 +14,7 @@ class PharmacyProvider {
     ApiRequest(url: 'https://api.collectapi.com/health/dutyPharmacy?ilce=$ilce&il=$il', data: null).get(
       beforeSend: () => {if (beforeSend != null) beforeSend()},
       onSuccess: (data) {
-        onSuccess!((data as List).map((postJson) => Result.fromJson(postJson)).toList());
+        onSuccess!((data['result'] as List).map((postJson) => Result.fromJson(postJson)).toList());
       },
       onError: (error) => {if (onError != null) onError(error)},
     );
